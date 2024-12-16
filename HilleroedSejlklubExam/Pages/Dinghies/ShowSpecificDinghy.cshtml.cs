6647fa1e0b40f10673354c441481cf3a822a9bf4
kaa004-zealand <kaa004@edu.zealand.dk>
2024-12-16 09:37:01 +0100

    Update

--- HilleroedSejlklubExam/Pages/Dinghies/ShowSpecificDinghy.cshtml.cs
+++ HilleroedSejlklubExam/Pages/Dinghies/ShowSpecificDinghy.cshtml.cs
@@ -1,14 +1,24 @@
 using HSLibrary.Interfaces;
+using HSLibrary.Models.Dinghy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using HSLibrary.Models.Dinghy;
 
 namespace HilleroedSejlklubExam.Pages.Dinghies
 {
     public class ShowSpecificDinghyModel : PageModel
     {
         IDinghyRepository _dinghyRepository;
-        public HSLibrary.Models.Dinghy.Dinghy Dinghy;
+        public Dinghy Dinghy;
+        #region NeedRepair
+        [BindProperty]
+        public string RepairComment { get; set; } = "";
+        #endregion
+        #region NeedRepair
+        [BindProperty]
+        public string Summary { get; set; } = "";
+        [BindProperty]
+        public string Notes { get; set; } = "";
+        #endregion
         public ShowSpecificDinghyModel(IDinghyRepository dinghyRepository)
         {
             _dinghyRepository = dinghyRepository;
@@ -16,6 +26,21 @@ namespace HilleroedSejlklubExam.Pages.Dinghies
         public void OnGet(int showId)
         {
             Dinghy = _dinghyRepository.Get(showId);
+        }
+        public IActionResult OnPostDelete(int deleteId)
+        {
+            _dinghyRepository.Remove(deleteId);
+            return RedirectToPage("ShowDinghies");
+        }
+        public void OnPostNeedRepair(int dinghyId)
+        {
+            Dinghy = _dinghyRepository.Get(dinghyId);
+            Dinghy.NeedRepair(RepairComment);
+        }
+        public void OnPostMakeRepair(int dinghyId)
+        {
+            Dinghy = _dinghyRepository.Get(dinghyId);
+            Dinghy.RepairDinghy(Summary, Notes);
         }
     }
 }
