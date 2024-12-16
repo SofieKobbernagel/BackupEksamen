5287a64aa0788615bac5f6153a53d7d8d9fc2f07
Mikkel Barfod <mib010@edu.zealand.dk>
2024-12-16 09:37:10 +0100

    add blog remove

--- HilleroedSejlklubExam/Pages/Blogs/RemoveBlog.cshtml.cs
+++ HilleroedSejlklubExam/Pages/Blogs/RemoveBlog.cshtml.cs
@@ -1,3 +1,5 @@
+using HSLibrary.Interfaces;
+using HSLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,21 @@ namespace HilleroedSejlklubExam.Pages.Blogs
 {
     public class RemoveBlogModel : PageModel
     {
-        public void OnGet()
+        private IBlogRepository _repo;
+
+        public Blog Blog { get; set; }
+        public RemoveBlogModel(IBlogRepository blogRepo)
         {
+            _repo = blogRepo;
+        }
+        public void OnGet(int deleteID)
+        {
+            Blog = _repo.Get(deleteID);
+        }
+        public IActionResult OnPost(int deleteId)
+        {
+            _repo.Remove(deleteId);
+            return RedirectToPage("ShowBlog");
         }
     }
 }
